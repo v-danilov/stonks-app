cf2466c2269222337fc78fd26d93f74074680f55
DanilovVA <Aa123456>
2020-06-18 12:03:17 +0300

    Manufacturer initial commit

---
+++ src/main/java/app/stonks/manufacturer/StonksAppManufacturerApplication.java
@@ -0,0 +1,13 @@
+package app.stonks.manufacturer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StonksAppManufacturerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StonksAppManufacturerApplication.class, args);
+	}
+
+}
