b83b94dbcd3d960cd7cd05147391b8ce47594430
DanilovVA <Aa123456>
2020-06-18 11:59:29 +0300

    Provider initial commit

---
+++ src/main/java/app/stonks/provider/StonksAppProviderApplication.java
@@ -0,0 +1,13 @@
+package app.stonks.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StonksAppProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StonksAppProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/stonks/provider/StonksAppProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package app.stonks.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StonksAppProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
