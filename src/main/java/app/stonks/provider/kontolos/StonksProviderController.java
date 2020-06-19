b8d85354b70bc34049180d14e77e2aa172e92050
DanilovVA <Aa123456>
2020-06-19 17:47:35 +0300

    Controller fix. MongoDB dep removed

--- src/main/java/app/stonks/provider/kontolos/StonksProviderController.java
+++ src/main/java/app/stonks/provider/kontolos/StonksProviderController.java
@@ -3,15 +3,17 @@ package app.stonks.provider.kontolos;
 import app.stonks.provider.bisness.IStonksOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/stonks")
+@RestController
+@RequestMapping("/stonks")
 public class StonksProviderController {
 
     @Autowired
     private IStonksOrderService stonksOrderService;
 
-    @GetMapping("random")
+    @GetMapping("/random")
     public void provideStonks() {
         stonksOrderService.orderRandomStonk();
     }
